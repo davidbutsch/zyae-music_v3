6fbadeb994a86e8231d874767e9596ed270135c0
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-29 13:13:46 -0500

    [client] feat: Set up vite project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import fs from "fs";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const httpsConfig = {
+  cert: "C:/ssl/certificate_data/__zyae_net.crt",
+  key: "C:/ssl/certificate_data/__zyae_net.key",
+};
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "zyae.net",
+    port: 3000,
+    https: {
+      key: fs.readFileSync(httpsConfig.key),
+      cert: fs.readFileSync(httpsConfig.cert),
+    },
+    open: true,
+  },
+});
