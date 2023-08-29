d42f2801c2f4e5076aebd847c9dfcf88474dfb7a
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-29 15:02:00 -0500

    [client] feat: Add user reducer

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/stores";
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps): JSX.Element => {
-  return <BrowserRouter basename="/music">{children}</BrowserRouter>;
+  return (
+    <BrowserRouter basename="/music">
+      <ReduxProvider store={store}>{children}</ReduxProvider>
+    </BrowserRouter>
+  );
 };
