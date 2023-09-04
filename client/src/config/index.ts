208875154bcc59a29668fef2f727f85c7e9e6855
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-04 10:01:17 -0500

    [client] feat: add asset urls to config

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,2 +1,9 @@
 export const AUTH_API_URL = "https://zyae.net/api";
 export const API_URL = "https://zyae.net/music/api";
+
+export const ASSETS_URL = "https://zyae.net/assets/";
+
+export const SVG_LOGO_URL =
+  "https://zyae.net/assets/images/brand/zyae/zm_logo.svg";
+export const PNG_LOGO_URL =
+  "https://zyae.net/assets/images/brand/zyae/zm_logo.png";
