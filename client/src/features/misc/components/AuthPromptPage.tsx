819260643128c54b1a93ccc28e103508d3b568db
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-05 17:45:28 -0500

    [client] feat: Add AuthPromptPage component

---
+++ client/src/features/misc/components/AuthPromptPage.tsx
@@ -0,0 +1,54 @@
+import { Box, SxProps, Typography, styled } from "@mui/material";
+import { FlaticonIcon, ImageIcon, LinkButton } from "@/components";
+
+type AuthPromptPageProps = {
+  icon?: string;
+  image?: string;
+  title: string;
+  subtext: string;
+  buttonText?: string;
+  sx?: SxProps;
+};
+
+const StyledBox = styled(Box)(({ theme }) =>
+  theme.unstable_sx({
+    mt: 12,
+
+    width: "100%",
+
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    textAlign: "center",
+  })
+);
+
+export const AuthPromptPage = ({
+  icon,
+  image,
+  title,
+  subtext,
+  buttonText = "Create account",
+  sx,
+}: AuthPromptPageProps): JSX.Element => {
+  return (
+    <StyledBox sx={{ ...sx }}>
+      {icon && <FlaticonIcon icon={icon} size={128} />}
+      {image && <ImageIcon url={image} size={128} />}
+      <Typography variant="h5" mt={3} mb={1}>
+        {title}
+      </Typography>
+      <Typography variant="subtitle1" color="text.secondary" mb={2}>
+        {subtext}
+      </Typography>
+      <LinkButton
+        to={`https://zyae.net/signup/?rd=${document.location.href}`}
+        variant="translucent"
+        sx={{ px: 2 }}
+      >
+        {buttonText}
+      </LinkButton>
+    </StyledBox>
+  );
+};
