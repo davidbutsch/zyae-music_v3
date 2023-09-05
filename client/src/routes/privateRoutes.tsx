003101cc194a0d1f505c37423e17f7a9cc250303
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-05 17:48:30 -0500

    [client] feat: Add private routes

--- client/src/routes/privateRoutes.tsx
+++ client/src/routes/privateRoutes.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Outlet, RouteObject } from "react-router-dom";
 
 import { AppLayout } from "@/components";
+import { Artist } from "@/features/artists";
+import { Typography } from "@mui/material";
 
 export const privateRoutes: RouteObject[] = [
   {
@@ -13,7 +15,27 @@ export const privateRoutes: RouteObject[] = [
     children: [
       {
         path: "/",
-        element: <>Zyae Music</>,
+        element: (
+          <Typography variant="h5">Welcome back (not implemented)</Typography>
+        ),
+      },
+      {
+        path: "/explore",
+        element: (
+          <Typography variant="h5">Explore (not implemented)</Typography>
+        ),
+      },
+      {
+        path: "/saved",
+        element: <Typography variant="h5">Saved (not implemented)</Typography>,
+      },
+      {
+        path: "/search",
+        element: <Typography variant="h5">Search (not implemented)</Typography>,
+      },
+      {
+        path: "/artist/:artistId",
+        element: <Artist />,
       },
       {
         path: "*",
