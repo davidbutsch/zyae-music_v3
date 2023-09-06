0c6b0afd432526f36e78d6b9428528babe15f65d
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-06 12:49:06 -0500

    [client] feat: Add QueryClientProvider to AppProvider

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { ThemeProvider } from "./ThemeProvider";
+import { queryClient } from "@/libs";
 import { store } from "@/stores";
 
 type AppProviderProps = {
@@ -12,7 +14,9 @@ export const AppProvider = ({ children }: AppProviderProps): JSX.Element => {
   return (
     <BrowserRouter basename="/music">
       <ReduxProvider store={store}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </QueryClientProvider>
       </ReduxProvider>
     </BrowserRouter>
   );
