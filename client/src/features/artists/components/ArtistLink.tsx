bbd3276f823c17d530b13b663406f587164fbc0f
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-11 18:30:54 -0500

    [client] feat Add artist link component

---
+++ client/src/features/artists/components/ArtistLink.tsx
@@ -0,0 +1,32 @@
+import { Box, Link, Typography } from "@mui/material";
+
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Track } from "@/features/tracks";
+import { colors } from "@/styles";
+
+export const ArtistLink = ({ artists }: { artists: Track["artists"] }) => {
+  return (
+    <Box
+      component="span"
+      sx={{
+        color: "text.secondary",
+        "*": {
+          color: `${colors.textSecondary} !important`,
+        },
+      }}
+    >
+      {artists.map((artist, i, { length }) => {
+        console.log(i + 1, length);
+        return (
+          <React.Fragment key={artist.id}>
+            <Link component={RouterLink} to={`/artist/${artist.id}`}>
+              {artist.name}
+            </Link>
+            {i + 2 < length ? ", " : i + 1 < length && " & "}
+          </React.Fragment>
+        );
+      })}
+    </Box>
+  );
+};
