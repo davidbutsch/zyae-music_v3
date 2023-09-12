977e350f18a2a9b68f32ab9a1f1610ab5e8bfa68
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-12 12:09:12 -0500

    [client] refact: Move AppLayout from index.ts to AppLayout.tsx

---
+++ client/src/components/layout/AppLayout.tsx
@@ -0,0 +1,24 @@
+import { CssBaseline, useMediaQuery } from "@mui/material";
+
+import { DesktopLayout } from "./DesktopLayout";
+import { MobileLayout } from "./MobileLayout";
+import { theme } from "@/styles";
+
+type AppLayoutProps = {
+  children: React.ReactNode;
+};
+
+export const AppLayout = ({ children }: AppLayoutProps): JSX.Element => {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <>
+      <CssBaseline />
+      {isSmallScreen ? (
+        <MobileLayout children={children} />
+      ) : (
+        <DesktopLayout children={children} />
+      )}
+    </>
+  );
+};
--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,26 +1,2 @@
-import { CssBaseline, useMediaQuery } from "@mui/material";
-
-import { DesktopLayout } from "./DesktopLayout";
-import { MobileLayout } from "./MobileLayout";
-import { theme } from "@/styles";
-
-type AppLayoutProps = {
-  children: React.ReactNode;
-};
-
+export * from "./AppLayout";
 export * from "./Content";
-
-export const AppLayout = ({ children }: AppLayoutProps): JSX.Element => {
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
-  return (
-    <>
-      <CssBaseline />
-      {isSmallScreen ? (
-        <MobileLayout children={children} />
-      ) : (
-        <DesktopLayout children={children} />
-      )}
-    </>
-  );
-};
