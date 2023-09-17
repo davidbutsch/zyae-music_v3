b9cc8f6e4a8973e05b84f2bac26a2c2e7b57acb9
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-17 13:37:48 -0500

    [client] feat: Add all filter for artist discography page

--- client/src/features/artists/components/DiscographyPage.tsx
+++ client/src/features/artists/components/DiscographyPage.tsx
@@ -14,7 +14,6 @@ import { useArtist, useArtistDiscography } from "@/features/artists";
 
 import { AlbumCards } from "@/features/albums";
 import React from "react";
-import { colors } from "@/styles";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
@@ -38,7 +37,7 @@ export const DiscographyPage = () => {
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleChangeType = (type: "albums" | "singles") => {
+  const handleChangeType = (type: "all" | "albums" | "singles") => {
     navigate(`../${artistId}/discography/${type}`);
     setAnchorEl(null);
   };
@@ -75,6 +74,14 @@ export const DiscographyPage = () => {
             "aria-labelledby": "basic-button",
           }}
         >
+          <MenuItem onClick={() => handleChangeType("all")}>
+            {type == "all" && (
+              <ListItemIcon>
+                <FlaticonIcon icon="fi fi-rr-check" />
+              </ListItemIcon>
+            )}
+            <ListItemText inset={type !== "all"}>All</ListItemText>
+          </MenuItem>
           <MenuItem onClick={() => handleChangeType("albums")}>
             {type == "albums" && (
               <ListItemIcon>
@@ -95,9 +102,13 @@ export const DiscographyPage = () => {
       </Stack>
       <AlbumCards
         carousel={false}
-        albums={discography.items.filter(
-          (item) => `${item.type?.toLowerCase()}s` == type
-        )}
+        albums={
+          type == "all"
+            ? discography.items
+            : discography.items.filter(
+                (item) => `${item.type?.toLowerCase()}s` == type
+              )
+        }
       />
     </Content>
   );
