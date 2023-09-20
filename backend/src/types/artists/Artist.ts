cf4158578d31376b1102ee9879816beb846b93a3
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-20 13:16:36 -0500

    [backend] refact: Set artist palette type to Palette

--- backend/src/types/artists/Artist.ts
+++ backend/src/types/artists/Artist.ts
@@ -1,28 +1,14 @@
 import { AlbumCard, ArtistCard, Track } from "@/types";
 
+import { Palette } from "../Palette";
+
 export interface Artist {
   id: string;
   description: string;
   views: string;
   name: string;
   saved: boolean;
-  palette: {
-    colors: {
-      hex: string;
-      red: number;
-      green: number;
-      blue: number;
-      area: number;
-      hue: number;
-      saturation: number;
-      lightness: number;
-      intensity: number;
-    }[];
-    byArea: { hex: string; determinant: number | null }[];
-    bySaturation: { hex: string; determinant: number | null }[];
-    byLightness: { hex: string; determinant: number | null }[];
-    byIntensity: { hex: string; determinant: number | null }[];
-  };
+  palette: Palette;
   thumbnails: {
     profile: {
       small: string;
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -6,3 +6,5 @@ export * from "./users";
 export * from "./artists";
 export * from "./albums";
 export * from "./tracks";
+
+export * from "./Palette";
