7ca75da535723f9028a42c0edd859bcef496c35d
David <102764748+davidbutsch@users.noreply.github.com>
2025-04-23 01:28:05 -0500

    do not check for unused params or imports

--- backend/src/loaders/schedule.ts
+++ backend/src/loaders/schedule.ts
@@ -1,8 +1,5 @@
-import { refreshGenres } from "@/jobs";
-
 export const scheduleLoader = async () => {
   try {
-    refreshGenres();
     // const job = scheduleJob("*/200 * * * * *", refreshGenres);
   } catch (err) {
     console.log(err);
